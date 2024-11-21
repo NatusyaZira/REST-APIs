b7802c101cf8fa8ad22f8c49505e6ac169a03bb8
Nataliia Kovalova <natusya.zira@outlook.com>
2024-11-21 19:21:02 +0200

    Create post request for new secrets

--- index.js
+++ index.js
@@ -4,9 +4,53 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+const yourBearerToken = "752824ef-bc71-4611-82ee-3cea9ae73140";
+const config = {
+  headers: { Authorization: `Bearer ${yourBearerToken}` },
+};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(posrt, () => {
+app.get("/", (req, res) => {
+    res.render("index.ejs", { content: "Waiting for data..." });
+  });
+
+app.post("/get-secret", async (req, res) => {
+    const searchId = req.body.id;
+    try {
+      const result = await axios.get(API_URL + "/secrets/" + searchId, config);
+      res.render("index.ejs", { content: JSON.stringify(result.data) });
+    } catch (error) {
+      res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
+  });
+  
+app.post("/post-secret", async (req, res) => {
+    // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+    try {
+        const result = await axios.post(API_URL + "/secrets", req.body, config);
+        res.render("index.ejs", {content: JSON.stringify(result.data)});
+    } catch (error) {
+        res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
+  });
+  
+app.post("/put-secret", async (req, res) => {
+    const searchId = req.body.id;
+    // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  });
+  
+app.post("/patch-secret", async (req, res) => {
+    const searchId = req.body.id;
+    // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+  });
+  
+app.post("/delete-secret", async (req, res) => {
+    const searchId = req.body.id;
+    // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
+  });
+app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
 })
